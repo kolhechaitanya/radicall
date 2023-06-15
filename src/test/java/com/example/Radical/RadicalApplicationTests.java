380f5631179594e60f513aa9e1c611ef41530da1
kolhechaitanya <kolhechaitanya1998@gmail.com>
2023-06-15 23:49:26 +0530

    code push to repository

---
+++ src/test/java/com/example/Radical/RadicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Radical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
